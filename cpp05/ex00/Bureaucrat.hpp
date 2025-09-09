2844afa66db51dacc2b0a344e3faf57e5b642c3a
Burskiii <yuangaoinberlin@gmail.com>
2025-09-09 18:51:32 +0200

    ex00 complete

--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -3,22 +3,45 @@
 
 #include <string>
 #include <iostream>
+#include <exception>
 
 class Bureaucrat {
 	public:
+		Bureaucrat();
 		Bureaucrat(const std::string &name, int grade);
-		Bureaucrat(const Bureaucrat(&other));
+		Bureaucrat(const Bureaucrat& other);
 		Bureaucrat &operator=(const Bureaucrat &other);
 		~Bureaucrat();
 
 		const std::string &getName() const;
 		int getGrade() const;
+
 		void incrementGrade();
 		void decrementGrade();
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw()
+				{
+					return ("Grade too high!");
+				}
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw()
+				{
+					return ("Grade too low!");
+				}
+		};
 
 	private:
 		const std::string name;
 		int grade;
-} ;
+};
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat& b);
 
 #endif
---
+++ cpp05/ex00/main.cpp
@@ -0,0 +1,57 @@
+#include "Bureaucrat.hpp"
+
+int main()
+{
+	Bureaucrat a("A", 1);
+	std::cout << a <<std::endl;
+
+	Bureaucrat b("B", 150);
+	std::cout << b <<std::endl;
+
+	std::cout << "Bureaucrat c('C', 0);" << std::endl;
+	try
+	{
+		Bureaucrat c("C", 0);
+		std::cout << c <<std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << "\n";
+	}
+
+
+	std::cout << "Bureaucrat d('D', 151);" << std::endl;
+	try
+	{
+		Bureaucrat d("D", 151);
+		std::cout << d <<std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << "\n";
+	}
+
+	std::cout << "increment_test from a" << std::endl;
+	try
+	{
+		a.incrementGrade();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << "\n";
+	}
+
+	std::cout << "decrement_test from b" << std::endl;
+	try
+	{
+		b.decrementGrade();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << "\n";
+	}
+
+	return 0;
+
+}
+#
