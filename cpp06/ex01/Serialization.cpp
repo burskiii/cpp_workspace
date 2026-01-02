5f7d6dcdeb58c261592a9066b305a7c5e9a4a891
Burskiii <yuangaoinberlin@gmail.com>
2026-01-02 15:57:33 +0100

    update

--- cpp06/ex01/Serialization.cpp
+++ cpp06/ex01/Serialization.cpp
@@ -11,5 +11,8 @@ Data* Serializer::deserialize(uintptr_t raw)
     return reinterpret_cast<Data*>(raw);
 }
 
-
+Serializer::Serializer(){}
+Serializer::~Serializer(){}
+Serializer::Serializer(const Serializer& other){}
+Serializer& Serializer::operator=(const Serializer& other){ return *this; }
 
